7d7c9716fb8957996d625aa37c021465a0c7a04e
Fellipe Augusto <Fellipe246@hotmail.com>
2025-12-19 18:02:54 -0300

    Adicionar arquivos de projeto.

---
+++ CurrencyConverter/Entities/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace CurrencyConverter.Entities
+{
+    internal class Converter(string name, double value)
+    {
+        public string Name { get; set; } = name;
+        public double Value { get; set; } = value;
+    
+
+
+    public double CoinConveter(string name, double value)
+        {
+            if (name == "EU")
+            {
+                return value * 5.54;
+            }
+            if(name == "USD")
+            {
+                return value * 6.49;
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,13 @@
+﻿using CurrencyConverter.Entities;
+using System.Globalization;
+
+Console.WriteLine("What currency do you want to convert to?");
+string coin = Console.ReadLine();
+
+Console.WriteLine("How much do you want to convert?");
+double value = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Converter converter = new Converter(coin, value);
+
+double result = converter.CoinConveter(coin, value);
+Console.WriteLine(result);
